8df97ef1fc8565884580184ea57db9a5c3764605
Dawson Riggs <dawsoncriggs@gmail.com>
2024-06-17 21:20:46 -0500

    5. Token Transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,10 +9,9 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
-    //Track Balances
     mapping(address => uint256) public balanceOf;
 
-    //Send Tokens
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     constructor(
         string memory _name,
@@ -23,5 +22,20 @@ contract Token {
         symbol = _symbol;
         totalSupply = _totalSupply * (10 ** decimals);
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0));
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
